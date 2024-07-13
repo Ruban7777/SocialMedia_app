d8f2424522cc05f6a2abc58473eb449dd01aa05d
Ruban7777 <rubanpais99@gmail.com>
2024-07-13 14:53:36 +0530

    spinner and useEffect hook added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,25 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
 
-  const handleGetPostClick = () => {
-    fetch("https://dummyjson.com/posts")
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
-  };
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage onGetPostClick={handleGetPostClick} />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -2,13 +2,6 @@ const WelcomeMessage = ({ onGetPostClick }) => {
   return (
     <center className="WelcomeMessage">
       <h1>There are no posts</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostClick}
-      >
-        Get Posts from server
-      </button>
     </center>
   );
 };
